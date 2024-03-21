34822a69edeb7e50bf5c254b917cb7b25bebd877
Taka <takahito.uchino.1218@gmail.com>
2024-03-21 11:49:39 +0900

    ABC 296 B

--- src/main.rs
+++ src/main.rs
@@ -1,51 +1,14 @@
 use proconio::input;
 fn main() {
     input! {
-        r: usize,
-        c: usize,
-        b: [String; r],
+        s: [String; 8],
     }
 
-    let mut blasted = vec![vec![false; c]; r];
-
-    for i in 0..r {
-        for j in 0..c {
-            if let Some(power) = b[i].chars().nth(j).unwrap().to_digit(10) {
-                for ni in 0..r {
-                    for nj in 0..c {
-                        if (max(i, ni) - min(i, ni)) + (max(j, nj) - min(j, nj)) <= power as usize {
-                            blasted[ni][nj] = true
-                        }
-                    }
-                }
+    for i in 0..8 {
+        for j in 0..8 {
+            if s[i].chars().nth(j).unwrap() == '*' {
+                println!("{}{}", (j as u8 + 97) as char, 8 - i);
             }
         }
-    }
-
-    for i in 0..r {
-        for j in 0..c {
-            if blasted[i][j] {
-                print!(".");
-            } else {
-                print!("{}", b[i].chars().nth(j).unwrap())
-            }
-        }
-        println!();
-    }
-}
-
-fn min(a: usize, b: usize) -> usize {
-    if a < b {
-        a
-    } else {
-        b
-    }
-}
-
-fn max(a: usize, b: usize) -> usize {
-    if a > b {
-        a
-    } else {
-        b
     }
 }
