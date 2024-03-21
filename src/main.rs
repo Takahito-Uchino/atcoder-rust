986e596e6e53e429c4537fb35c3cb5f3bac421b9
Taka <takahito.uchino.1218@gmail.com>
2024-03-21 11:39:57 +0900

    ABC 295 B

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,51 @@
 use proconio::input;
-
 fn main() {
     input! {
-        h: usize,
-        w: usize,
-        a: [[u8; w]; h],
+        r: usize,
+        c: usize,
+        b: [String; r],
     }
 
-    for i in 0..h {
-        for j in 0..w {
-            if a[i][j] == 0 {
-                print!(".")
-            } else {
-                print!("{}", (a[i][j] + 64) as char)
+    let mut blasted = vec![vec![false; c]; r];
+
+    for i in 0..r {
+        for j in 0..c {
+            if let Some(power) = b[i].chars().nth(j).unwrap().to_digit(10) {
+                for ni in 0..r {
+                    for nj in 0..c {
+                        if (max(i, ni) - min(i, ni)) + (max(j, nj) - min(j, nj)) <= power as usize {
+                            blasted[ni][nj] = true
+                        }
+                    }
+                }
             }
         }
-        println!()
+    }
+
+    for i in 0..r {
+        for j in 0..c {
+            if blasted[i][j] {
+                print!(".");
+            } else {
+                print!("{}", b[i].chars().nth(j).unwrap())
+            }
+        }
+        println!();
+    }
+}
+
+fn min(a: usize, b: usize) -> usize {
+    if a < b {
+        a
+    } else {
+        b
+    }
+}
+
+fn max(a: usize, b: usize) -> usize {
+    if a > b {
+        a
+    } else {
+        b
     }
 }
