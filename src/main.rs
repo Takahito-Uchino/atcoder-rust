3318e3aed7787e1f38f05f18135e76719d8f4400
Taka <takahito.uchino.1218@gmail.com>
2024-03-21 10:53:42 +0900

    ABC 291 B

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,22 @@
-// -*- coding:utf-8-unix -*-
-
 use proconio::input;
-
-// ABC086C - Traveling
-// https://atcoder.jp/contests/abs/tasks/arc089_a
 
 fn main() {
     input! {
         n: usize,
-        mut plan: [(i32, i32, i32); n],  // Vec<(i32, i32, i32)>
+        mut x: [usize; 5 * n],
     }
-    plan.insert(0, (0, 0, 0));
-    let yes = plan.windows(2).all(|w| {
-        let (t0, x0, y0) = w[0];
-        let (t1, x1, y1) = w[1];
-        let time = t1 - t0;
-        let dist = (x1 - x0).abs() + (y1 - y0).abs();
-        dist <= time && time % 2 == dist % 2
-    });
-    println!("{}", if yes { "Yes" } else { "No" });
+
+    for _ in 0..n {
+        let max = x.iter().max().unwrap();
+        let max_index = x.iter().position(|&v| v == *max).unwrap();
+        x.remove(max_index);
+    }
+
+    for _ in 0..n {
+        let min = x.iter().min().unwrap();
+        let min_index = x.iter().position(|&v| v == *min).unwrap();
+        x.remove(min_index);
+    }
+
+    println!("{}", x.iter().sum::<usize>() as f64 / (3 * n) as f64)
 }
