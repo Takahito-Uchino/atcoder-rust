374ea0689742f108c1490af3ea77a424921d011a
Taka <takahito.uchino.1218@gmail.com>
2024-03-23 22:05:03 +0900

    ABC 346 C

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,23 @@
+use std::{collections::HashSet, iter::FromIterator};
+
 use proconio::input;
 
 fn main() {
     input! {
         n: usize,
+        k: usize,
         a: [usize; n],
     }
 
-    for i in 0..n - 1 {
-        print!("{} ", a[i] * a[i + 1]);
+    let mut total = 0;
+
+    let a_set: HashSet<usize> = HashSet::from_iter(a.iter().cloned());
+
+    for i in a_set {
+        if k >= i {
+            total += i;
+        }
     }
 
-    println!();
+    println!("{}", (1 + k) * k / 2 - total);
 }
