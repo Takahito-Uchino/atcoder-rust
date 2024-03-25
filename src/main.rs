2bc170f6ec16477e4106d9283977fbf82ac14937
Taka <takahito.uchino.1218@gmail.com>
2024-03-25 19:10:10 +0900

    ABC 315 B

--- src/main.rs
+++ src/main.rs
@@ -2,44 +2,20 @@ use proconio::input;
 
 fn main() {
     input! {
-        n: usize,
+        m: usize,
+        d: [usize; m],
     }
 
-    let mut a = Vec::new();
+    let mid = (d.iter().sum::<usize>() + 1) / 2;
 
-    for _ in 0..n {
-        input! {
-            c: usize,
-            vets: [usize; c],
+    let mut current = 0;
+
+    for i in 0..m {
+        if mid > current && mid <= current + d[i] {
+            println!("{} {}", i + 1, mid - current);
+            return;
+        } else {
+            current += d[i];
         }
-        a.push(vets);
-    }
-
-    input! {
-        x: usize,
-    }
-
-    let mut is_vet = Vec::new();
-
-    for i in 0..n {
-        if a[i].contains(&x) {
-            is_vet.push(i);
-        }
-    }
-
-    let mut cmin = 37;
-    for i in &is_vet {
-        cmin = cmin.min(a[*i].len());
-    }
-
-    let mut answer = Vec::new();
-    for i in &is_vet {
-        if a[*i].len() == cmin {
-            answer.push(i + 1);
-        }
-    }
-    println!("{}", answer.len());
-    for ans in answer {
-        print!("{} ", ans);
     }
 }
