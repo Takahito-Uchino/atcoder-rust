66c36afee569c7cbf75ed913acdb72330f9c13c1
Taka <takahito.uchino.1218@gmail.com>
2024-03-25 19:28:03 +0900

    ABC 318 B

--- src/main.rs
+++ src/main.rs
@@ -3,14 +3,48 @@ use proconio::input;
 fn main() {
     input! {
         n: usize,
-        mut a: [usize; n],
     }
 
-    a.sort();
+    let mut a = Vec::new();
+    let mut b = Vec::new();
+    let mut c = Vec::new();
+    let mut d = Vec::new();
 
-    for i in 0..n - 1 {
-        if a[i] + 1 != a[i + 1] {
-            println!("{}", a[i] + 1);
+    for _ in 0..n {
+        input! {
+            an: usize,
+            bn: usize,
+            cn: usize,
+            dn: usize,
+        }
+        a.push(an);
+        b.push(bn);
+        c.push(cn);
+        d.push(dn);
+    }
+
+    let b_max = b.iter().max().unwrap();
+    let d_max = d.iter().max().unwrap();
+
+    let mut s = vec![vec![false; *d_max]; *b_max];
+
+    for i in 0..n {
+        for j in a[i]..b[i] {
+            for k in c[i]..d[i] {
+                s[j][k] = true;
+            }
         }
     }
+
+    let mut answer = 0;
+
+    for s1 in s {
+        for s2 in s1 {
+            if s2 {
+                answer += 1;
+            }
+        }
+    }
+
+    println!("{}", answer);
 }
