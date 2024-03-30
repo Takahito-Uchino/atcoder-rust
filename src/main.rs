2c0744c8361b0e1ded3de15208dbb4a80e4571f0
Taka <takahito.uchino.1218@gmail.com>
2024-03-30 21:03:36 +0900

    ABC 347 A

--- src/main.rs
+++ src/main.rs
@@ -1,41 +1,17 @@
-use proconio::{input, marker::Chars};
+use proconio::input;
 
 fn main() {
     input! {
         n: usize,
-        s: [Chars; n],
+        k: usize,
+        a: [usize; n],
     }
 
-    let mut wins = vec![0; n];
-
-    for i in 0..n {
-        for j in 0..n {
-            if s[i][j] == 'o' {
-                wins[i] += 1;
-            }
+    for i in a {
+        if i % k == 0 {
+            print!("{} ", i / k);
         }
     }
 
-    let mut bucket = vec![Vec::new(); n];
-
-    for i in 0..n {
-        bucket[wins[i]].push(i);
-    }
-
-    let mut answer = Vec::new();
-
-    for i in (0..n).rev() {
-        for &j in &bucket[i] {
-            answer.push(j + 1);
-        }
-    }
-
-    println!(
-        "{}",
-        answer
-            .iter()
-            .map(|x| x.to_string())
-            .collect::<Vec<String>>()
-            .join(" ")
-    );
+    println!()
 }
