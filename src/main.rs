a00c90d2133c5bd87d8908480dd7a13e81d53d41
Taka <takahito.uchino.1218@gmail.com>
2024-04-02 11:21:11 +0900

    ABC 347 B

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,35 @@
-use proconio::input;
+use proconio::{input, marker::Chars};
 
 fn main() {
     input! {
-        n: usize,
+        s: Chars,
     }
 
-    for x in 0..=n {
-        for y in 0..=n {
-            for z in 0..=n {
-                if x + y + z <= n {
-                    println!("{} {} {}", x, y, z);
-                }
-            }
+    let (mut count_a, mut count_b, mut count_c) = (0, 0, 0);
+
+    for c in &s {
+        match c {
+            'A' => count_a += 1,
+            'B' => count_b += 1,
+            'C' => count_c += 1,
+            _ => unreachable!(),
         }
+    }
+
+    let mut answer = Vec::new();
+    for _ in 0..count_a {
+        answer.push('A');
+    }
+    for _ in 0..count_b {
+        answer.push('B');
+    }
+    for _ in 0..count_c {
+        answer.push('C');
+    }
+
+    if &answer == &s {
+        println!("Yes");
+    } else {
+        println!("No");
     }
 }
