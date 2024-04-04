eb0b66ec251f7ce5ce20b4c82132904af12a5788
Taka <takahito.uchino.1218@gmail.com>
2024-04-04 12:57:53 +0900

    ABC 340 B

--- src/main.rs
+++ src/main.rs
@@ -2,39 +2,17 @@ use proconio::input;
 
 fn main() {
     input! {
-        h: usize,
-        w: usize,
-        n: usize,
+        q: usize,
+        queries: [(usize, usize); q],
     }
 
-    let mut grid = vec![vec![false; w]; h];
+    let mut a = Vec::new();
 
-    let dx: Vec<isize> = vec![-1, 0, 1, 0];
-    let dy: Vec<isize> = vec![0, 1, 0, -1];
-
-    let (mut x, mut y) = (0isize, 0isize);
-    let mut m: isize = 0;
-
-    for _ in 0..n {
-        let ux = x.rem_euclid(h as isize) as usize;
-        let uy = y.rem_euclid(w as isize) as usize;
-
-        if grid[ux][uy] == false {
-            grid[ux][uy] = true;
-            m += 1;
+    for query in queries {
+        if query.0 == 1 {
+            a.push(query.1);
         } else {
-            grid[ux][uy] = false;
-            m += 3;
+            println!("{}", a[a.len() - query.1]);
         }
-        m %= 4;
-        x += dx[m as usize];
-        y += dy[m as usize];
-    }
-
-    for i in 0..h {
-        for j in 0..w {
-            print!("{}", if grid[i][j] { '#' } else { '.' })
-        }
-        println!();
     }
 }
