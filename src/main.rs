13f8bd522f9ce0aa4b17ff6bece18a16277afb12
Taka <takahito.uchino.1218@gmail.com>
2024-04-06 17:59:45 +0900

    A09

--- src/main.rs
+++ src/main.rs
@@ -2,31 +2,39 @@ use proconio::input;
 
 fn main() {
     input! {
+        h: usize,
+        w: usize,
         n: usize,
-        xy: [(usize, usize); n],
-        q: usize,
-        abcd: [(usize, usize, usize, usize); q],
+        abcd: [(usize, usize, usize, usize); n],
     }
 
-    let mut z = vec![vec![0; 1509]; 1509];
+    let mut s = vec![vec![0; w + 2]; h + 2];
 
-    for (x, y) in xy {
-        z[x][y] += 1;
+    for (a, b, c, d) in abcd {
+        s[a][b] += 1;
+        s[c + 1][d + 1] += 1;
+        s[c + 1][b] -= 1;
+        s[a][d + 1] -= 1;
     }
 
-    let mut t = vec![vec![0; 1509]; 1509];
-    for i in 1..1509 {
-        for j in 1..1509 {
-            t[i][j] = t[i][j - 1] + z[i][j];
+    let mut t = vec![vec![0; w + 2]; h + 2];
+
+    for i in 1..=h {
+        for j in 1..=w {
+            t[i][j] = t[i][j - 1] + s[i][j];
         }
     }
-    for j in 1..1509 {
-        for i in 1..1509 {
+
+    for j in 1..=w {
+        for i in 1..=h {
             t[i][j] += t[i - 1][j];
         }
     }
 
-    for (a, b, c, d) in abcd {
-        println!("{}", t[c][d] + t[a - 1][b - 1] - t[a - 1][d] - t[c][b - 1]);
+    for i in 1..=h {
+        for j in 1..=w {
+            print!("{} ", t[i][j]);
+        }
+        println!()
     }
 }
