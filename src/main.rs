3e6b92702f92e5bafb03f457f6a7232dbf8f52b1
Taka <takahito.uchino.1218@gmail.com>
2024-04-06 21:33:57 +0900

    ABC 348 C

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,28 @@
+use std::collections::HashMap;
+
 use proconio::input;
 
 fn main() {
     input! {
         n: usize,
+        ac: [(usize, usize); n],
     }
 
-    for i in 1..=n {
-        if i % 3 == 0 {
-            print!("x");
-        } else {
-            print!("o")
+    let mut mins = HashMap::new();
+
+    for (a, c) in ac {
+        mins.entry(c).or_insert(a);
+        if mins[&c] > a {
+            mins.insert(c, a);
         }
     }
-    println!()
+
+    let mut ans = 0;
+    for (_, v) in mins {
+        if v > ans {
+            ans = v;
+        }
+    }
+
+    println!("{}", ans);
 }
