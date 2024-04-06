bf9f7a9ca9553b740dba9a7854283271ea1ef94f
Taka <takahito.uchino.1218@gmail.com>
2024-04-06 21:02:54 +0900

    ABC 348 A

--- src/main.rs
+++ src/main.rs
@@ -3,26 +3,14 @@ use proconio::input;
 fn main() {
     input! {
         n: usize,
-        a: [usize; n],
-        d: usize,
-        lr: [(usize, usize); d],
     }
 
-    let mut p = vec![0; n + 1];
-
-    p[1] = a[0];
-    for i in 2..=n {
-        p[i] = p[i - 1].max(a[i - 1]);
+    for i in 1..=n {
+        if i % 3 == 0 {
+            print!("x");
+        } else {
+            print!("o")
+        }
     }
-
-    let mut q = vec![0; n + 1];
-
-    q[n] = a[n - 1];
-    for i in (1..n).rev() {
-        q[i] = q[i + 1].max(a[i - 1]);
-    }
-
-    for (l, r) in lr {
-        println!("{}", p[l - 1].max(q[r + 1]));
-    }
+    println!()
 }
