96830522ad65b3e2f08d585d6fbeef0889d6a5c2
Taka <takahito.uchino.1218@gmail.com>
2024-04-10 13:54:32 +0900

    B16

--- src/main.rs
+++ src/main.rs
@@ -3,16 +3,15 @@ use proconio::input;
 fn main() {
     input! {
         n: usize,
-        a: [usize; n - 1],
-        b: [usize; n - 2],
+        h: [isize; n],
     }
 
     let mut dp = Vec::new();
     dp.push(0);
-    dp.push(a[0]);
+    dp.push((h[1] - h[0]).abs());
 
     for i in 2..n {
-        dp.push((dp[i - 1] + a[i - 1]).min(dp[i - 2] + b[i - 2]));
+        dp.push((dp[i - 1] + (h[i - 1] - h[i]).abs()).min(dp[i - 2] + (h[i - 2] - h[i]).abs()));
     }
 
     println!("{}", dp[n - 1]);
